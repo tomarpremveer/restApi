563423a6736c94acb511e033ac11c220a1262500
tomarmonty <montytomar78@gmail.com>
2020-07-09 22:54:29 +0530

    models updated

--- app.py
+++ app.py
@@ -1,155 +1,95 @@
 from flask import Flask,render_template,jsonify,abort,request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from functools import wraps
 #from flask_cors import CORS
 app=Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
-app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:7500Monty###@localhost/api"
+app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:7500Monty###@localhost/restApi"
 db=SQLAlchemy(app)
 migrate=Migrate(app,db)
 #CORS(app)
-class Parent(db.Model):
-    __tablename__="parents"
+class Movie(db.Model):
+    __tablename__="movies"
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(20),nullable=False)
-    age=db.Column(db.Integer,nullable=False)
-    childrens=db.relationship("Child",backref="parent",lazy=True,cascade="all,delete",passive_deletes=True)
+    runtime=db.Column(db.Integer,nullable=False)
+    release_date=db.Column(db.Date)
+    reviews=db.relationship("Review",backref="parent",lazy=True,cascade="all,delete",passive_deletes=True)
     
     def format(ins):
         return jsonify({
-            "success":True,
-            "name":ins.name,
-            "age":ins.age
+            "success":True
         })
-class Child(db.Model):
-    __tablename__="childrens"
+class Review(db.Model):
+    __tablename__="reviews"
     id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(20),nullable=False)
-    age=db.Column(db.Integer,nullable=False)
-    parent_id=db.Column(db.Integer,db.ForeignKey('parents.id',ondelete="cascade"),nullable=False)
+    review_text=db.Column(db.String(20))
+    movie_id=db.Column(db.Integer,db.ForeignKey('movies.id',ondelete="cascade"),nullable=False)
     
         
     def format(ins):
         return jsonify({
-            "success":True,
-            "name":ins.name,
-            "age":ins.age
+            "success":True
         })
+class User(db.Model):
+    __tablename__="users"
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(10),nullable=False)
+    username=db.Column(db.String(10),nullable=False)
+    password=db.Column(db.String(10),nullable=False)
   #index page  
 @app.route('/')
 def index():
     return "Welcome to the api page"
 
+def token(f):
+    @wraps(f)
+    def wrapper(*args,**kwargs):
+        token=None
+
+        if 'x-access-token' in request.headers:
+            token=request.headers['x-access-token']
+        if not token:
+            return jsonify({'message':'Token is missing'}),401
+        return f(*args,**kwargs)
+    return wrapper
+
 #api section
-
-#1.Get parent by Id . Id should be an Integer
-@app.route('/parents/<int:parent_id>/',methods=['GET'])
-def get_parent(parent_id=None):
-    if parent_id==None:
-        return jsonify({
-            "success":False,
-            "message":"Parent id isn't specififed."
-        })
+#1.CRUD endpoints related to User
+@app.route('/create/user',methods=['POST'])
+def create_user():
     try:
-        parent=Parent.query.filter(Parent.id==parent_id).one_or_none()
-        if parent is None:
-            abort(404)
-            
-        return parent.format()
+        data=request.args.get('name','Default Value')
+        return jsonify({'message':data})
     except:
-        abort(503)
+        return jsonify({'message':'hlo'})
+@app.route('/delete/user/<int:id>',methods=['DELETE'])
+def delete_user():
+    pass
+@app.route('/update/user',methods=['PUT','PATCH'])
+def update_user():
+    pass
+#2.create a movie
+@app.route('/create/movie',methods=['POST'])
+def create_movie():
+    return jsonify({'message':'you have successfully reached movie creation route'})
+#3.Delete a movie
+@app.route('/delete/movie/<int:id>',methods=['DELETE'])
+def delete_movie(id):
+    pass
+#Reviews endpoint
 
-#2.Create a Parent
-@app.route('/parents/',methods=['POST'])
-def create_parent():
-    data=request.get_json()
-    if data is None:
-        abort(404)
-    else:
-        parent=Parent(name=data["name"],age=data["age"])
-        db.session.add(parent)
-        db.session.commit()
-        return jsonify({
-            "sucess":True,
-            "message":"Record successfully created",
-            "code":200
-        })
-# Delete a Parent using the Parent Id.
-@app.route('/parents/<int:parent_id>/delete/',methods=["DELETE"])
-def delete_parent(parent_id):
-    try:
-        parent=Parent.query.filter(Parent.id==parent_id).one_or_none()
-        if parent is None:
-            return jsonify({
-            "success":False,
-            "message":"Invalid parent Id",
-            "error_code":404
-            })
-        temp=parent
-        db.session.delete(parent)
-        db.session.commit()
-        return temp.format()
-    except:
-        db.session.rollback()
-    finally:
-        db.session.close()
-#3. create a child . Accepted methods POST . Url:/parent/<parent_id>/child/create/
-@app.route('/parents/<int:parent_id>/childrens/',methods=["POST"])
-def create_child(parent_id):
-    data=request.get_json()
-    data_keys=[i for i in data.keys()]
-    parent=Parent.query.filter(Parent.id==parent_id).one_or_none()
-    if parent is None:
-        return jsonify({
-            "success":False,
-            "message":"Invalid parent Id",
-            "error_code":404
-        })
-    if "name" not in data_keys:
-        return jsonify({
-            "success":False,
-            "message":"Invalid input format.name and age both should be provided in the input"
-        })
-    if "age" not in data_keys:
-        return jsonify({
-            "success":False,
-            "message":"Invalid input format.name and age both should be provided in the input"
-        })
-    else:
-        try:
-            child=Child(name=data["name"],age=data["age"])
-            child.parent=parent
-            db.session.add(child)
-            db.session.commit()
-            return jsonify({
-                "success":True,
-                "message":"Child added succcessfully"
-            })
-        except:
-            abort(503)
-            db.session.rollback()
-        finally:
-            db.session.close()
+#4.Create a review
 
-#4. get all the childrens of a parent by parent Id
-@app.route('/parents/<int:parent_id>/childrens/')
-def get_chidlren_of_parent(parent_id):
-    parent=Parent.query.filter(Parent.id==parent_id).one_or_none()
-    if parent is None:
-        return jsonify({
-            "success":False,
-            "message":"Invalid parent Id",
-            "error_code":404
-        })
-    else:
-        try:
-            childrens=Child.query.filter(Child.parent_id==parent_id).order_by(Child.age).all()
-            return childrens[0].format()
-        except:
-            abort(500)
+@app.route('/create/review',methods=['POST'])
+def create_review():
+    pass
 
-
-
+#5.Delete review
+@app.route('/delete/movie/<int:movie_id/review/<int:review_id>',methods=['DELETE'])
+def delete_review():
+    pass
 # error handler section
 
 @app.errorhandler(404)
